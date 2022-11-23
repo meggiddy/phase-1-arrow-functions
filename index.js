1325e821b5a1d8eb18ac0a1911b4485b735fb2e2
Maggie <muriithinyawira13@gmail.com>
2022-11-23 12:05:58 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (a =2000, b =100) => a / b;
+//chose to have default since test has no passing parameters
+const square = (x) => {
+    let result= x*=x;
+    return result;
+}
+const add = (a, b) => a+b;
